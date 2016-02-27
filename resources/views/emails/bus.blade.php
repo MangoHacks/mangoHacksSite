35a4bc82c06fafc1067f09c9d688196d7cee91ef
Juan Alvarado <jalvaradl91@gmail.com>
2016-02-27 15:35:46 -0500

    bus email

---
+++ resources/views/emails/bus.blade.php
@@ -0,0 +1,42 @@
+<div class="email-bg" style="background: #20396D;padding: 30px 0px 30px 0px;color: white;font-family: 'Helvetica', sans-serif;">
+	<div class="pre-head" style="max-width: 600px;margin: 0 auto;padding: 10px 20px 10px 20px;background: #20396D;font-size: 10px;color: white;">
+	    <a href="https://www.eventbrite.com/e/northflo-takes-mangohacks-tickets-22254248055" style="color: #00E35C;">MangoHacks NorthFlo Bus!</a>
+	</div>
+	<div class="header" style="max-width: 480px;margin: 0 auto;padding: 10px 20px;">
+	    <img src="http://mangohacks.com/img/logo.png" alt="MRE" style="max-width: 100%;">
+	</div>
+	<div class="email-container" style="max-width: 600px;margin: 0 auto;padding: 0px 20px 20px 20px;background: #20396D;">
+	    <p class="greeting" style="color: white;font-size: 36px;font-weight: bold;">Reserve your bus seat!</p>
+	  
+	    <p>
+	      The NorthFlo Bus is coming. There are a limited amount of seats, so get yours at the link bellow. 
+	      
+	      The <strong>$20 deposit will be refunded after you check into MangoHacks and check into bus on both ways.</strong>
+	    </p>
+	    
+	    <p>
+	    We want to make sure the people that really need the bus to get MangoHacks are able to get a seat.
+	  </p>
+	  
+	    <p>
+	   If you only need to travel one way, find someone that's also attending the event that can take your spot on the other way.</p>
+	    
+	    <a class="cto" href="https://www.eventbrite.com/e/northflo-takes-mangohacks-tickets-22254248055" style="color: white;display: block;margin: 24px auto;width: 240px;background: #00E35C;border-radius: 5px;text-align: center;text-decoration: none;font-weight: bold;padding: 16px 24px;font-size: 20px;">Reserve a Seat</a>
+	    <p>
+	      If you haven't already, Join our <a href="" style="color: #00E35C;">Attendees Facebook Group</a> to start meeting other hackers, organize carpools, share gifs, and get the HYPE going. 
+	    </p>
+	  
+	    <p>
+	      If you still have any questions, hit us up at <a href="mailto:team@mangohacks.com" style="color: #00E35C;">team@mangohacks.com</a> or on <a href="https://www.facebook.com/MangoHacks" style="color: #00E35C;">Facebook</a> and <a href="https://twitter.com/fiumangohacks" style="color: #00E35C;">Twitter</a>. 
+	    </p>
+	  
+	    <p>&lt;3</p>
+	    <p>The MangoHacks Team</p>
+	    
+	    <div class="footer" style="background-color: #20396D;margin-top: 10px;color: white;padding: 5px;font-size: 11px;">
+	        <p>
+	            &copy; 2016 MangoHacks
+	        </p>
+	    </div>
+	</div>
+</div>
